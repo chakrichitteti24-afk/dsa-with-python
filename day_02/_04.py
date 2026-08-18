4ab80432f1509a27517addbf97aa9bc31bb2df34
chakri <chakri@example.com>
2026-08-18 21:25:37 +0530

    this my my second commit

---
+++ day_02/_04.py
@@ -0,0 +1,14 @@
+n = int(input("enter your number"))
+
+if n<=1:
+    print("the number is not prime")
+else:
+    prime= True
+    for i in range(2,n):
+        if n%i ==0:
+            prime = False
+            break
+    if prime:
+        print("the number is prime number")
+    else:
+        print("the number is not prime number")
