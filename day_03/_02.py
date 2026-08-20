f2aa3b093efd55634995920ac37808bd512b9f8a
chakri <chakri@example.com>
2026-08-20 12:41:43 +0530

    this is my day 3 od dsa series

---
+++ day_03/_02.py
@@ -0,0 +1,6 @@
+a = int(input("enter your first number:"))
+b=  int(input("enter your second number:"))
+
+while b!=0:
+    a,b =b,a%b
+print("GCD:",a)
---
+++ day_03/_05.py
@@ -0,0 +1,8 @@
+n = int(input("enter youir numbers:"))
+
+sum = 0
+while n>0:
+    digit = n%10
+    sum = sum+digit
+    n=n//10
+print("the sum of digits is ", sum)
